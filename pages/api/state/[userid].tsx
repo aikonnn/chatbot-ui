7c864b948eee1ca46a46524c4bc02cd44889b469
aikonnn <nokia_tuk@hotmail.com>
2023-05-31 09:25:57 +0700

    minor bug fix

--- pages/api/state/[userid].tsx
+++ pages/api/state/[userid].tsx
@@ -8,7 +8,7 @@ export default async function handleUserState(req: NextApiRequest, res: NextApiR
         if(ans.rows.length === 0){
             //add to table
             await client.query("INSERT INTO userState(userid, apiKey, showChatbar, showPromptbar, selectedConversation) values($1::uuid, '', true, true, NULL)",[req.query.userid]);
-            ans = await client.query(`SELECT apiKey, showChatbar, showPromptbar, selectedConversation from userState where email='${req.query.userid}'`);
+            ans = await client.query(`SELECT apiKey, showChatbar, showPromptbar, selectedConversation from userState where userid='${req.query.userid}'`);
         }
 
         return res.status(200).json({...ans.rows[0]})
