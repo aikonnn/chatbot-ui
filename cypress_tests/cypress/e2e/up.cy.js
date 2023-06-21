dd2f0ce7dd406c89b4e20da189442b9bf78fa34f
aikonnn <nokia_tuk@hotmail.com>
2023-06-22 01:13:41 +0700

    test login cookie not found

--- cypress_tests/cypress/e2e/up.cy.js
+++ cypress_tests/cypress/e2e/up.cy.js
@@ -1,15 +1,36 @@
 describe('Site Status', () => {
   it('visit main page', () => {
+    Cypress.Cookies.debug(true)
+
     cy.visit('http://chatgpt:3000/')
     cy.url().should("includes", "/auth/login")
     cy.contains('Login with DEX').click()
+    
+
     cy.origin('http://dex:5556/', () => {
       cy.url().should("include", "/dex/auth")
       cy.get('#password').type('password')
       cy.get('#login').type('admin@example.com')
       cy.get('#submit-login').click()
 
+      cy.wait(2500)
+
       cy.get(".dex-btn.theme-btn--success").click()
     })
+
+    cy.getCookies()
+    .should('have.length', 4)
+    .then((cookies) => {
+      cy.log(cookies[0].name)
+      cy.log(cookies[1].name)
+      cy.log(cookies[2].name)
+      cy.log(cookies[3].name)
+    })
+
+    cy.wait(10000)
+
+    cy.url().should('equal', 'http://chatgpt:3000/')
+    
+
   })
 })
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -35,6 +35,6 @@ export const authOptions = {
   ],
   pages: {
     signIn: "/auth/login"
-  }
+  },
 }
 export default NextAuth(authOptions)
