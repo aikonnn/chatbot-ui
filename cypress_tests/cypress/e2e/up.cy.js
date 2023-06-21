74df3da905e39efac57587e590044eb2a39c97fd
aikonnn <nokia_tuk@hotmail.com>
2023-06-21 14:40:37 +0700

    login test partly resolved

--- cypress_tests/cypress/e2e/up.cy.js
+++ cypress_tests/cypress/e2e/up.cy.js
@@ -3,6 +3,13 @@ describe('Site Status', () => {
     cy.visit('http://chatgpt:3000/')
     cy.url().should("includes", "/auth/login")
     cy.contains('Login with DEX').click()
-    cy.url().should("include", "/dex/auth")
+    cy.origin('http://dex:5556/', () => {
+      cy.url().should("include", "/dex/auth")
+      cy.get('#password').type('password')
+      cy.get('#login').type('admin@example.com')
+      cy.get('#submit-login').click()
+
+      cy.get(".dex-btn.theme-btn--success").click()
+    })
   })
 })
